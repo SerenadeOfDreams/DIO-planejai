01c12c94bc0a9938ecc07996c38d58c42afcdfaa
Arthur da Silva dos Santos <asdsantos@sercom.com.br>
2026-07-02 17:49:31 -0300

    feat + refactor: adicionados no formulário de solicitação estados de navegação dos steps. / utilização do StepProgress e do FormStep agora está dinâmica.

--- src/features/simulation/Form.tsx
+++ src/features/simulation/Form.tsx
@@ -1,14 +1,39 @@
+import { useState } from "react";
 import { simulationFormSteps } from "../../data/simulation";
 import { FormStep } from "./FormStep";
 import { StepProgress } from "./Progress";
 
 export const SimulationForm = () => {
-  const currentStep = simulationFormSteps[5];
+  const [currenStepIndex, setCurrentStepIndex] = useState(0);
+  const totalSteps = simulationFormSteps.length;
+  const currentStep = simulationFormSteps[currenStepIndex];
+
+  const handleNextStep = () => {
+    if (currenStepIndex + 1 > totalSteps - 1) {
+      return;
+    }
+
+    setCurrentStepIndex((prev) => prev + 1);
+  };
+
+  const handlePreviousStep = () => {
+    if (currenStepIndex === 0) {
+      return;
+    }
+
+    setCurrentStepIndex((prev) => prev - 1);
+  };
 
   return (
     <>
-      <StepProgress currentStep={6} totalSteps={6} />
-      <FormStep key={currentStep.id} {...currentStep} />
+      <StepProgress currentStep={currenStepIndex + 1} totalSteps={totalSteps} />
+      <FormStep
+        key={currentStep.id}
+        {...currentStep}
+        onBack={handlePreviousStep}
+        onNext={handleNextStep}
+        hideBackButton={currenStepIndex == 0}
+      />
     </>
   );
 };
