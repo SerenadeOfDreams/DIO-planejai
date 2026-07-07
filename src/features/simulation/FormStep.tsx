a72df03f7397f5301dfcc8a81ed8470d13661c73
Arthur da Silva dos Santos <asdsantos@sercom.com.br>
2026-07-07 11:50:02 -0300

    feat: adicionadas formatação e validação do valor dos campos do formulário de solicitação.

--- src/features/simulation/FormStep.tsx
+++ src/features/simulation/FormStep.tsx
@@ -2,6 +2,7 @@ import { ArrowLeft, ArrowRight, type LucideIcon } from "lucide-react";
 import { useState, type SyntheticEvent } from "react";
 import { Button } from "../../components/shared/Button";
 import { Input, type InputProps } from "../../components/shared/Input";
+import { formatCurrencyMask } from "../../utils/currency";
 
 export interface FormStepProps {
   id: string;
@@ -58,7 +59,13 @@ export function FormStep({
         <Input
           {...inputProps}
           value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
+          onChange={(e) =>
+            setInputValue(
+              inputProps.prefix == "R$"
+                ? formatCurrencyMask(e.target.value)
+                : e.target.value,
+            )
+          }
         />
         <div className="flex flex-col gap-3 sm:flex-row sm:gap-6">
           {!hideBackButton && (
