a74e57fc3dab9acb18b184be59f1011f20266c0a
Arthur da Silva dos Santos <asdsantos@sercom.com.br>
2026-07-21 17:14:52 -0300

    feat: criado o hook useChat para enviar o promt da mensagem à i.a.

---
+++ src/hooks/useChat.tsx
@@ -0,0 +1,63 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import { BuildAIChatPrompt } from "../data/aiChatPromt";
+import type { SimulationRecord } from "../data/simulation";
+import { getChatAnswer, type ChatData } from "../services/aiService";
+import { useSimulationStorage } from "./useSimulationStorage";
+
+export const useChat = (id: string) => {
+  const { getFormData, updateSimulation } = useSimulationStorage();
+  const [chat, setChat] = useState<ChatData | null>(() => {
+    const simulation = getFormData(id);
+
+    if (simulation?.chatData) return simulation.chatData;
+
+    return null;
+  });
+  const [chatIsLoading, setChatIsLoading] = useState(false);
+  const [chatError, setChatError] = useState<string | null>(null);
+  const isRequestPending = useRef(false);
+  const [userText, setUserText] = useState("");
+
+  //Esse useCallBack é para passar a fetchChat como array de dependências da useEffect abaixo
+  const fetchChat = useCallback(
+    async (simulationId: string, text: string) => {
+      const simulation = getFormData(simulationId);
+
+      if (!simulation) {
+        setChatError("Simulação não encontrada");
+        return;
+      }
+
+      isRequestPending.current = true;
+      setChatIsLoading(true);
+      setChatError(null);
+      setUserText(text);
+
+      try {
+        const prompt = BuildAIChatPrompt(simulation, simulation.insight!, text);
+        const data = await getChatAnswer(prompt);
+        setChat(data);
+
+        updateSimulation(simulationId, {
+          ...simulation,
+          chatData: data,
+        } as SimulationRecord);
+      } catch {
+        setChatError("Erro ao gerar uma resposta. Tente novamente.");
+      } finally {
+        isRequestPending.current = false;
+        setChatIsLoading(false);
+      }
+    },
+    [getFormData, updateSimulation],
+  );
+
+  useEffect(() => {
+    //Evita loop infinito de requisição para a API do Gemini
+    if (chat || chatIsLoading || chatError || isRequestPending.current) return;
+
+    fetchChat(id, userText);
+  }, [id, chat, chatIsLoading, chatError, fetchChat]);
+
+  return { chat, chatIsLoading, chatError, fetchChat };
+};
