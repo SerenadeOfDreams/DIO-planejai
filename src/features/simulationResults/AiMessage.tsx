09d267c45e177e8f37a3246b0073bcd8821a0c36
Arthur da Silva dos Santos <asdsantos@sercom.com.br>
2026-07-23 14:34:29 -0300

    feat: adicionados textos para melhor identificação das mensagens.

--- src/features/simulationResults/AiMessage.tsx
+++ src/features/simulationResults/AiMessage.tsx
@@ -4,7 +4,10 @@ export function AiMessage({ message }: MessageProps) {
   return (
     <div className="pr-20 lg:pr-90">
       <div className="bg-(--color-skeleton-highlight) rounded-2xl p-3 text-muted-foreground text-sm leading-relaxed break-words">
-        {message}
+        <div className="flex flex-col gap-2">
+          <p className="text-xs text-(--color-primary)">Agente de I.A</p>
+          {message}
+        </div>
       </div>
     </div>
   );
--- src/features/simulationResults/UserMessage.tsx
+++ src/features/simulationResults/UserMessage.tsx
@@ -4,7 +4,10 @@ export function UserMessage({ message }: MessageProps) {
   return (
     <div className="pl-20 lg:pl-90">
       <div className="bg-(--color-skeleton-base) rounded-2xl p-3 text-muted-foreground text-sm leading-relaxed break-words">
-        {message}
+        <div className="flex flex-col gap-2">
+          <p className="text-xs text-(--color-primary)">Você</p>
+          {message}
+        </div>
       </div>
     </div>
   );
